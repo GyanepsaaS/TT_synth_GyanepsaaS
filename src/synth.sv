7720ab6084c73f204615be3a48c9487c4eba0f9b
Gyanepsaa Singh <64652949+GyanepsaaS@users.noreply.github.com>
2023-11-01 02:59:51 -0400

    Update synth.sv

--- src/synth.sv
+++ src/synth.sv
@@ -12,6 +12,7 @@ module tt_um_synth_GyanepsaaS
     (input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
